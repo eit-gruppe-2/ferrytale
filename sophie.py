32ee69266be706b7ce96349c1064e4ef03e9b224
sophiesberg <sophiesberg@gmail.com>
2019-02-20 08:33:16 +0100

    Her er det spillet

--- sophie.py
+++ sophie.py
@@ -1,11 +1,6 @@
-"""
-Sample Python/Pygame Programs
-Simpson College Computer Science
-http://programarcadegames.com/
-http://simpson.edu/computer-science/
-"""
-
 import pygame
+pygame.init()
+
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -15,23 +10,28 @@ RED = (255, 0, 0)
 BLUE=(0,128,180)
 
 
+# class ferry(pygame.sprite.Sprite):
+#     def __init__(self):
+#         pygame.sprite.Sprite.__init__(self)
+#         ferry.rect = rect()
+
 def draw_stick_figure(screen, x, y):
 
-    #make screen blue
     screen.fill(BLUE)
-    # Head
-    pygame.draw.ellipse(screen, BLACK, [1 + x, y, 20, 20], 0)
+    pygame.draw.rect(screen, BLACK, [1 + x, y, 20, 20], 0)
 
-
-
-# Setup
-pygame.init()
+def draw_boat(boat, x, y):
+    pygame.draw.rect(boat, GREEN, [x, y, 20, 20], 0)
 
 # Set the width and height of the screen [width,height]
-size = [700, 500]
-screen = pygame.display.set_mode(size)
 
-pygame.display.set_caption("My Game")
+display_width = 700
+display_height = 500
+
+size = [display_width, display_height]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Ferrytale")
+
 
 # Loop until the user clicks the close button.
 done = False
@@ -42,13 +42,21 @@ clock = pygame.time.Clock()
 # Hide the mouse cursor
 pygame.mouse.set_visible(0)
 
+xb = 450
+yb = 0
+
+
+
 # Speed in pixels per frame
 x_speed = 0
 y_speed = 0
-
+speed = 3
 # Current position
 x_coord = 10
 y_coord = 10
+
+
+
 
 # -------- Main Program Loop -----------
 while not done:
@@ -78,13 +86,44 @@ while not done:
             elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 y_speed = 0
 
+    # while not crashed:
+    #for event in pygame.event.get()
+
+
     # --- Game Logic
 
     # Move the object according to the speed vector.
     x_coord = x_coord + x_speed
     y_coord = y_coord + y_speed
 
-    # Check if boundary or land
+    yb = yb + speed
+
+    if yb == 480:
+        speed = - speed
+
+    if yb == 0:
+        speed = -speed
+
+    if x_coord == xb:
+        done = True
+    elif y_coord == yb:
+        done = True
+
+    # y_prev= 0
+    #
+    # while y_prev <= yb:
+    #     y_prev = yb
+    #     yb = yb + 3
+    #     if yb == 480:
+    #         yb = yb - 3
+    #
+    # while y_prev > yb:
+    #      y_prev = yb
+    #      yb = yb - 3
+    #      if yb == 0:
+    #         y_prev = yb - 1
+
+
     if x_coord < 0:
         x_coord = 0
     elif x_coord > 680:
@@ -92,7 +131,7 @@ while not done:
     if y_coord < 0:
         y_coord = 0
     elif y_coord > 480:
-       y_coord = 480
+        y_coord = 480
 
     # --- Drawing Code
 
@@ -101,6 +140,8 @@ while not done:
 
 
     draw_stick_figure(screen, x_coord, y_coord)
+
+    draw_boat(screen, xb, yb)
 
 
     # Go ahead and update the screen with what we've drawn.
